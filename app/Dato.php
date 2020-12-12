407cef6a89727839f60a60b5fe229079d4cd63d4
Richardace <richardacevedo98@gmail.com>
2020-12-12 17:27:54 -0500

    cambios

---
+++ app/Dato.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dato extends Model
+{
+    protected $table = 'datos';
+
+    protected $fillable = [
+        'id', 'nombre', 'direccion'
+    ];
+}
